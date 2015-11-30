a42081550ea061650fde1487cf7d0a495dabfac5
PT <PT@PTs-MacBook-Pro.local>
2015-11-30 09:01:51 -0700

    Checkpoint Foundation 19 Storyboarding Alocater Completed

---
+++ Alcolater/Alcolater/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Alcolater
+//
+//  Created by PT on 11/30/15.
+//  Copyright (c) 2015 PeterTanner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Alcolater/Alcolater/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Alcolater
+//
+//  Created by PT on 11/30/15.
+//  Copyright (c) 2015 PeterTanner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
