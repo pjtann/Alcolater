e3efdb71eb3b26ed6b885fc2cd8a40bc8af6e643
PT <PT@PTs-MacBook-Pro.local>
2015-11-30 19:43:51 -0700

    Checkpoint Foundation 20 Implementing Calculations Completed

---
+++ Alcolater/Alcolater/WhiskeyViewController.h
@@ -0,0 +1,13 @@
+//
+//  WhiskeyViewController.h
+//  Alcolater
+//
+//  Created by PT on 11/30/15.
+//  Copyright (c) 2015 PeterTanner. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface WhiskeyViewController : ViewController
+
+@end
