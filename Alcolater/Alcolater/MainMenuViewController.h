ec11671cad4aadd9324290e8cc1a172ac993e4a3
PT <PT@PTs-MacBook-Pro.local>
2015-12-01 18:51:52 -0700

    Checkpoint Foundation 21 Basic Navigation Completed

---
+++ Alcolater/Alcolater/MainMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainMenuViewController.h
+//  Alcolater
+//
+//  Created by PT on 12/1/15.
+//  Copyright (c) 2015 PeterTanner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainMenuViewController : UIViewController
+
+@end
